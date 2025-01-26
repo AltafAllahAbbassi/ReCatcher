ef5b7c4349f3dc169d5608f4e45ed295c3762840
altaf <altafallahabbassi@gmail.com>
2025-01-26 12:58:38 -0500

    merge all generations

--- code_generation/constants.py
+++ code_generation/constants.py
@@ -12,3 +12,12 @@ BIGCODEBENCH = "bigcode"
 FREQUENCE_PENALITY=0, 
 PRESENCE_PENALITY=0
 
+_2_GENERATE_INPUT_SYSTEM_PROMPT = """
+Generate an appropriate input for a given Python method signature and docstring that would allow for measuring its time and memory usage effectively.
+Ensure the input is complex enough to potentially reflect the time and memory consumption of the code upon execution.
+Please return **ONLY** function call with the generated input.
+
+# Notes:
+- **Size Consideration**: Remember that inputs should be large but not infinitely large. Keep in mind the memory constraints of a typical system.
+- **Data Types**: Make sure to use appropriate data types, e.g., strings, integers, lists, dictionaries, etc., congruent with the intended use of the provided code snippet.
+"""
