2ea1f348020225f42184f090c03851b930541087
Altaf-Allah Abbassi <p117619@slurm.info.polymtl.ca>
2025-02-02 12:20:31 -0500

    update pmd path

--- ReCatcher/constants.py
+++ ReCatcher/constants.py
@@ -7,7 +7,7 @@ BENCHMARKS = {
 BENCHMARK_METHOD_CALLS = {
     
 }
-PMD_COMMAND_TEMPLATE = "/home/altaf/Documents/pmd-dist-7.8.0-bin/pmd-bin-7.8.0/bin/pmd cpd --minimum-tokens={} -d {} -f text --language python"
+PMD_COMMAND_TEMPLATE = "/usagers4/p117619/projects/ReCatcher/pmd-bin-7.8.0/bin/pmd cpd --minimum-tokens={} -d {} -f text --language python"
 CODE_DUPLICATION_MIN_COUNT = 10
 COMMENT_DUPLICATION_THRESHOLD = 0.9 
 COMMENT_DUPLICATION_MIN_COUNT = 4
