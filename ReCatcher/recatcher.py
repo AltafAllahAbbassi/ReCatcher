cca55bf3511949bee7372e6bb0617ea3eb072bca
Altaf-Allah Abbassi <p117619@slurm.info.polymtl.ca>
2025-02-02 12:23:10 -0500

    edits recatcher

--- ReCatcher/recatcher.py
+++ ReCatcher/recatcher.py
@@ -12,7 +12,7 @@ import matplotlib
 matplotlib.use('Agg') 
 
 class ReCatcher(object):
-    def __init__(self, benchmark=BENCHMARKS["HUMANEVAL_PLUS"], n_rep = 5, large_timeout=600, test_timeout=120):
+    def __init__(self, benchmark=BENCHMARKS["HUMANEVAL_PLUS"], n_rep = 5, large_timeout=600, test_timeout=100):
         self.n_rep = n_rep # how many times we repeat the execution for perforamnce
         self.large_timeout = large_timeout
         self.test_timeout = test_timeout
@@ -27,7 +27,6 @@ class ReCatcher(object):
             self.benchmark_name = "bigcodebench"
 
     def test_regression(self, result1_df, result2_df, method, result_dir, large_input=True, timeout=True):
-        os.makedirs(result_dir, exist_ok=True)
         ## static  properties
         if method in [unnecessary_conditional_block, unnecessary_else, variable_naming, sub_readable, syntax_error, syntax_error, missing_import_declaration, code_duplication, comment_duplication]:
             result_file1, summary1 = self.test_static(result1_df, method=method, result_dir=result_dir)
@@ -54,11 +53,10 @@ class ReCatcher(object):
             print(f"Results saved in: {result_file}")            
             return results, results
         
-            
-        else:
-            return "Test not supported"
         
         if method == "general_logic" or method in [unnecessary_conditional_block, unnecessary_else, variable_naming, sub_readable, syntax_error, syntax_error, missing_import_declaration, code_duplication, comment_duplication]:
+            print(summary1)
+            print(summary2)
             mean1= mean(x1)
             mean2 = mean(x2)
             print(f"Mean1: {mean1}")
@@ -74,17 +72,24 @@ class ReCatcher(object):
             print(f"effect_size: {effect_}")
             return summary1, summary2
         
+        if method == "measure_performance":
+            x11, x21 = self.measure_performance(result_df=result1_df, result_dir=result_dir)
+            print(x11, x21)
+            
+            x12, x22 = self.measure_performance(result_df=result2_df, result_dir=result_dir)
+            print(x12, x22)
+            
+            return ((x11, x21), (x12, x22))
+            
     def test_static(self, result_df,  method, result_dir):
-        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}.json")
-        os.makedirs(result_dir, exist_ok=True)
+        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}_{method}.json")
         assert len(result_df) == len(self.benchmark)
         n_rep = len([x for x in result_df.columns if x.startswith("exp")])
         results = {}
         summary = {}
         for j in tqdm(range(n_rep)):
             result = []
-            for i in tqdm(range(5)):
-            # for i in tqdm(range(len(result_df))):
+            for i in tqdm(range(len(result_df))):
                 task_id = result_df["task_id"][i]
                 prompt = result_df["prompt"][i]
                 code = result_df[f"exp_{str(j)}"][i]
@@ -123,10 +128,9 @@ class ReCatcher(object):
             return self.test_general_logic_bigcode_bench(result_df, result_dir)
 
     def test_general_logic_humaneval(self, result_df, result_dir):
-        os.makedirs(result_dir, exist_ok=True)
         assert len(result_df) == len(self.benchmark)
         n_rep = len([x for x in result_df.columns if x.startswith("exp")])
-        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}.jsonl")
+        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}_logic.jsonl")
         summary = {}
         results = {}
         for j in tqdm(range(n_rep)):
@@ -144,11 +148,7 @@ class ReCatcher(object):
                     "code": code,
                     "test": test
                 }
-                try:
-                    exec(test)
-                    to_save["correct_code"] = True
-                except:
-                    to_save["correct_code"] = False
+                to_save["correct_code"] = execute_assert_tests(test, self.test_timeout)
                 write_jsonl_line(result_file, to_save)
                 result.append(to_save)
             results[f"exp_{j}"] = result
@@ -156,16 +156,14 @@ class ReCatcher(object):
         return result_file, summary
     
     def test_general_logic_bigcode_bench(self, result_df, result_dir):
-        os.makedirs(result_dir, exist_ok=True)
-        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}.jsonl")
+        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}_logic.jsonl")
         assert len(result_df) == len(self.benchmark)
         n_rep = len([x for x in result_df.columns if x.startswith("exp")])
         results = {}
         summary = {}
         for j in range(n_rep):
             result = []
-            # for i in tqdm(range(len(result_df))):
-            for i in tqdm(range(5)):
+            for i in tqdm(range(len(result_df))):
                 task_id = result_df["task_id"][i]
                 prompt = result_df["prompt"][i]
                 code = result_df[f"exp_{str(j)}"][i]
@@ -287,63 +285,106 @@ class ReCatcher(object):
         summary["memory"] = dict(Counter(memory_result))
         return  result_file, summary
     
+    def measure_performance(self, result_df, result_dir):
+        correct_code = 0
+        n_rep = len([x for x in result_df.columns if x.startswith("exp")])
+        result_file = os.path.join(result_dir, f"{str(uuid.uuid4())}_performance.json")
+        avg_times = []
+        avg_memories = []
+        for i in tqdm(range(len(result_df))):
+            task_id = result_df["task_id"][i]
+            prompt = result_df["prompt"][i]
+            memory = []
+            time = []
+            
+            for j in range(n_rep):
+                # forming unit test of human eval 
+                code = result_df[f"exp_{str(j)}"][i]
+                
+                if self.benchmark_name == "humaneval":
+                    method_name = extract_method_name(code)[0]
+                    test = code + "\n" + self.benchmark[i]["test"] + "\n" + f"check({method_name})"
+                    method = execute_assert_tests
 
+                elif self.benchmark_name == "bigcodebench":
+                    test =code + "\n"+ self.benchmark[i]["test"] + f'\nimport io\noutput = io.StringIO()  # Create a StringIO buffer to capture the output\nloader = unittest.TestLoader()\nsuite = loader.loadTestsFromTestCase(TestCases)\nrunner = unittest.TextTestRunner(verbosity=2, stream=output)  # Direct output to the buffer\nrunner.run(suite)\ntest_output = output.getvalue()  # Get the captured output'
+                    method = execute_unittest
+                performance = measure_execution_performance(code=test, method=method, n_repetition=self.n_rep, timeout=self.test_timeout)
+                if not(performance["execution_time"] =="FAIL"  or performance["memory_usage"] =="FAIL"):
+                        time.extend(performance["execution_time"])
+                        memory.extend(performance["memory_usage"])
+            if len(time) and len(memory):
+                correct_code+=1
+                avg_mem = mean(memory)
+                avg_time = mean(time)
+                avg_memories.append(avg_mem)
+                avg_times.append(avg_time)
+                to_save = {
+                    "task_id": task_id,
+                    "prompt": prompt,
+                    "memory": memory, 
+                    "time": time, 
+                    "avg_mem" : avg_mem, 
+                    "avg_time": avg_time
+                    }
+                write_jsonl_line(result_file, to_save)
+    
+        return result_file, {"avg_time": mean(avg_times), "avg_mem": mean(avg_memories), "n_correct_codes": correct_code}
 
 
 if __name__ == "__main__":
-    large_input = False
-    timeout = True
-    # /workspace/ReCatcher/
-    # /workspace/ReCatcher/data
-    re_catcher = ReCatcher("/home/altaf/Desktop/ReCatcher/data/bigcode/dataset.parquet", test_timeout=45)
-    gpt3_5 = pd.read_csv("/home/altaf/Desktop/ReCatcher/code_generation/results/merged_bigcodebench/deepseek-ai_deepseek-coder-6.7b-base.csv")
-    gpt4 = pd.read_csv("/home/altaf/Desktop/ReCatcher/code_generation/results/merged_bigcodebench/DevQuasar_coma-7B-v0.1.csv")
-    # gpt4mini = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/gpt-4o-mini.csv")
-    
-    # codellama_base = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/meta-llama_CodeLlama-7b-hf.csv")
-    # codellama_finetuned = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/JetBrains_CodeLlama-7B-Kexer.csv")
-    # codellama_merged = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/DevQuasar_coma-7B-v0.1.csv")
-    
-    # deepseek_base = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/deepseek-ai_deepseek-coder-6.7b-base.csv")
-    # deepseek_finetuned = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/JetBrains_deepseek-coder-6.7B-kexer.csv")
-    # deepseek_merged = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_bigcodebench/ori-cloud_ds-trinity-7b-v1.csv")
+    re_catcher = ReCatcher("/usagers4/p117619/projects/ReCatcher/data/humaneval_plus/HumanEvalPlus.jsonl", test_timeout=45)
+    model1 = pd.read_csv("/usagers4/p117619/projects/ReCatcher/code_generation/results/merged_humaneval_plus/meta-llama_CodeLlama-7b-hf.csv")
+    model2 = pd.read_csv("/usagers4/p117619/projects/ReCatcher/code_generation/results/merged_humaneval_plus/DevQuasar_coma-7B-v0.1.csv")
+    result_dir = "/usagers4/p117619/projects/ReCatcher/results/humanevalplus/codellama_base_merged"
+    # os.makedirs(result_dir)
     
     
-    # re_catcher = ReCatcher(benchmark="/workspace/ReCatcher/data/humaneval_plus/HumanEvalPlus.jsonl")
-    # gpt3_5 = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/gpt-3.5-turbo.csv")
-    # gpt4 = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/gpt-4o.csv")
-    # gpt4mini = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/gpt-4o-mini.csv")
+    # print("###### general logic ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, "general_logic", result_dir)
+    # print(x1, x2)
     
-    # codellama_base = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/meta-llama_CodeLlama-7b-hf.csv")
-    # codellama_finetuned = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/JetBrains_CodeLlama-7B-Kexer.csv")
-    # codellama_merged = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/DevQuasar_coma-7B-v0.1.csv")
+    # print("###### Performance ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, "measure_performance", result_dir)
+    # print(x1, x2)
     
-    # deepseek_base = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/deepseek-ai_deepseek-coder-6.7b-base.csv")
-    # deepseek_finetuned = pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/JetBrains_deepseek-coder-6.7B-kexer.csv")
-    # deepseek_merged =pd.read_csv("/workspace/ReCatcher/code_generation/results/merged_humaneval_plus/ori-cloud_ds-trinity-7b-v1.csv")
+    
+    # print("###### syntax error ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, syntax_error, result_dir)
+    # print(x1, x2)
+    
+    
+    # print("###### declaration import ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, missing_import_declaration, result_dir)
+    # print(x1, x2)
+    
+    
+    # print("###### code duplication ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, code_duplication, result_dir)
+    # print(x1, x2)
+    
+    
+    # print("###### comment duplication ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, comment_duplication, result_dir)
+    # print(x1, x2)
+    
+    
+    # print("###### unnecessary else ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, unnecessary_else, result_dir)
+    # print(x1, x2)
+    
+    
+    
+    # print("###### unnecessary conditional block ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, unnecessary_conditional_block, result_dir)
+    # print(x1, x2)
     
 
-    # x1, x2 = re_catcher.test_regression(result1_df= codellama_base, result2_df=codellama_finetuned, method="performance", large_input=large_input, timeout=timeout, result_dir="results")
-    # print("codellama base vs finetuned")
+    # print("###### confusing variable naming ######")
+    # x1, x2 = re_catcher.test_regression(model1, model2, variable_naming, result_dir)
     # print(x1, x2)
     
-    # x1, x2 = re_catcher.test_regression(result1_df= codellama_base, result2_df=codellama_merged, method="performance", large_input=large_input, timeout=timeout, result_dir="results")
-    # print("codellama base vs merged")
-    # print(x1, x2)
     
-    # x1, x2 = re_catcher.test_regression(result1_df= deepseek_base, result2_df=deepseek_finetuned, method="performance", large_input=large_input, timeout=timeout, result_dir="results")
-    # print("deepseek base vs finetuned")
-    # print(x1, x2)
-    
-    # x1, x2 = re_catcher.test_regression(result1_df= deepseek_base, result2_df=deepseek_merged, method="performance", large_input=large_input, timeout=timeout, result_dir="results")
-    # print("deepseek base vs merged")
-    # print(x1, x2)
-    
-    x1, x2 = re_catcher.test_regression(result1_df= gpt3_5, result2_df=gpt4, method=unnecessary_else, large_input=large_input, timeout=timeout, result_dir="results")
-    print("gpt 3.5 vs gpt 4")
+    print("###### sub-readable code ######")
+    x1, x2 = re_catcher.test_regression(model1, model2, sub_readable, result_dir)
     print(x1, x2)
-    
-    # x1, x2 = re_catcher.test_regression(result1_df= gpt4, result2_df=gpt4mini, method="performance", large_input=large_input, timeout=timeout, result_dir="results")
-    # print("gpt 4 vs gpt 4o mini")
-    # print(x1, x2)
-    
